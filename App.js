80d8949f7984916378cd63b15ab9c342cabd856d
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-12 12:12:29 +0100

    Header component

--- App.js
+++ App.js
@@ -1,18 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
-// import CarItem from "./components/CarItem";
 import CarsList from "./components/CarsList";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
+
       <CarsList />
-      {/* <CarItem
-        name="Model 3"
-        tagline="Order Online for"
-        taglineCTA="Touchless Delivery"
-        image={require("./assets/images/Model3.jpeg")}
-      /> */}
+
       <StatusBar style="auto" />
     </View>
   );
