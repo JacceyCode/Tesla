edc187485c14e94973f5c3f1f969d816522a5bc0
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-12 11:27:54 +0100

    Car Item component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -2,20 +2,19 @@ import { View, Text, ImageBackground } from "react-native";
 import styles from "./styles";
 import StyledButton from "../StyledButton";
 
-function CarItem() {
+function CarItem({ name, tagline, taglineCTA, image }) {
   return (
     <View style={styles.carContainer}>
-      <ImageBackground
-        source={require("../../assets/images/ModelX.jpeg")}
-        style={styles.image}
-      />
+      <ImageBackground source={image} style={styles.image} />
 
       <View style={styles.titles}>
-        <Text style={styles.title}>Model S</Text>
-        <Text style={styles.subtitle}>Starting at $69,420</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+          {tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+        </Text>
       </View>
 
-      <View>
+      <View style={styles.buttonsContainer}>
         <StyledButton
           type="primary"
           onPress={() => {
